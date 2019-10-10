7e6f1ef90a8c861a3fe90ec197f87f3807a61985
Natasha Valentina <natasha.valentina1998@gmail.com>
2019-10-10 22:46:56 +0700

    add detail Activity and share

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,27 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private TextView weatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        weatherTextView = (TextView) findViewById(R.id.tv_weather);
+
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+        String weatherData = intent.getStringExtra("weather");
+        weatherTextView.setText(weatherData);
+
+
     }
 }
--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -20,6 +20,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -222,7 +223,28 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         }
 
         // TODO (2) Launch the map when the map menu item is clicked
+        else if (id == R.id.action_map) {
+            String addressString =  "1600 Ampitheatre Parkway, CA";
+            openMap(addressString);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openMap(String location){
+        Uri uri = new Uri.Builder()
+                .scheme("geo")
+                .path("0,0")
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(uri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.e(TAG, "Error opening Map");
+        }
     }
 }
